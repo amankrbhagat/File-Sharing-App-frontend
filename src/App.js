b480de79c56f8cb273e3303a6976c3c7e41f696b
Aman Kumar Bhagat <bhagatamankumar1999@gmail.com>
2023-10-13 11:14:41 +0530

    file-sharing-app

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
     <div className='container'>
       <img src={logo} alt="banner" />
       <div className='wrapper'>
-        <h1>Simple file sharing!</h1>
+        <h1>File Sharing App!</h1>
         <p>Upload and share the download link.</p>
         
         <button onClick={() => onUploadClick()}>Upload</button>
